ae5ce3378c5ff0fa597cab5879711a3a4e219d3e
Fatwn Alsalmi <fatwn.alsalmi@kcl.ac.uk>
2023-11-28 15:37:22 +0000

    Modify forms.py

--- things/forms.py
+++ things/forms.py
@@ -4,15 +4,8 @@ from .models import Thing
 class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
-        exclude = ['created_at']
-
-    widgets = {
-        'description': forms.Textarea(attrs={'rows': 4, 'cols': 15, 'maxlength': 120}),
-        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 100}),
-    }
-
-    def clean_quantity(self):
-        quantity = self.cleaned_data.get('quantity')
-        if quantity < 0 or quantity > 100:
-            raise forms.ValidationError("Quantity must be between 0 and 100.")
-        return quantity
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(attrs={'maxlength': '120'}),
+            'quantity': forms.NumberInput(),
+        }
