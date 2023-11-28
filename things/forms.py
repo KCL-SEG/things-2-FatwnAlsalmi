d965359fa0a529dbd99574ad9a9df34da880519a
Fatwn Alsalmi <fatwn.alsalmi@kcl.ac.uk>
2023-11-28 15:26:01 +0000

    Fix errors

--- things/forms.py
+++ things/forms.py
@@ -4,18 +4,15 @@ from .models import Thing
 class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
-        # Exclude the 'created_at' field from the form
         exclude = ['created_at']
 
-    # Customize the widgets for specific fields
     widgets = {
         'description': forms.Textarea(attrs={'rows': 4, 'cols': 15}),
-        'quantity': forms.NumberInput(attrs={'min': 1}),
+        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 100}),
     }
 
-    # Add any additional validation if needed
     def clean_quantity(self):
         quantity = self.cleaned_data.get('quantity')
-        if quantity < 0:
-            raise forms.ValidationError("Quantity cannot be negative.")
+        if quantity < 0 or quantity > 100:
+            raise forms.ValidationError("Quantity must be between 0 and 100.")
         return quantity
